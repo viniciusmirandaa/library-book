27145994b3adff2b0a8983acab9bc57a5c392a5f
vinicius <vini.s.miranda@hotmail.com>
2022-12-30 00:11:47 -0300

    [ADD] changes related to read_group function

--- __manifest__.py
+++ __manifest__.py
@@ -14,6 +14,7 @@
         "views/library_book.xml",
         "views/res_partner.xml",
         "views/library_member.xml",
+        "views/library_book_categ.xml",
         ],
     'installable': True,
     'application': True,
--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from . import library_book
 from . import res_partner
 from . import res_partner_prototype
 from . import library_member
+from . import library_book_categ
